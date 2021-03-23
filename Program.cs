95f13f2ca4073ffffe17007e6fc7d89804c5d57e
student <Student@B420-01.fesb.hr>
2021-03-23 18:27:15 +0100

    Prvi commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
